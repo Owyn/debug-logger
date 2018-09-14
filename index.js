a345c2f95c9f6b1122cbb0bd5ab52a932495850b
Owyn <Owyn@users.noreply.github.com>
2018-09-14 21:03:19 +0300

    16kb write buffer now

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const path = require('path'),
 	fs = require('fs')
 
 module.exports = function DebugLogger(dispatch) {
-	let file = path.join(__dirname, '..', '..', 'tera-proxy-' + Date.now() + '.log')
-
+	let filepath = path.join(__dirname, '..', '..', 'tera-proxy-' + Date.now() + '.log')
+	let file = fs.createWriteStream(filepath, {highWaterMark: 1024*1024});
+	
+	process.on('exit', ()=> {
+		file.end('<---- TERA proxy TERMINATED ---->\r\n');
+	});
+	
+	this.destructor = function()
+	{
+		file.end('<---- TERA proxy UNLOADED ---->\r\n');
+	}
+	
 	dispatch.hook('*', 'raw', { order: 999999, filter: { fake: null, silenced: null, modified: null} }, (code, data, fromServer, fake) => {
 		let today = new Date();
-	fs.appendFile(file, "[" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds() + "]	" + (fromServer ? '<-' : '->') + '	' + (fake ? '[CRAFTED]	' : '') + (data.$silenced ? '[BLOCKED]	' : '') + (data.$modified ? '[EDITED]	' : '') + ( (!fake && !data.$silenced && !data.$modified) ? '         	' : '') + (dispatch.base.protocolMap.code.get(code) || code) + '	' + data.toString('hex') + '\n', (function(){}))
+	file.write("[" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds() + "]	" + (fromServer ? '<-' : '->') + '	' + (fake ? '[CRAFTED]	' : '') + (data.$silenced ? '[BLOCKED]	' : '') + (data.$modified ? '[EDITED]	' : '') + ( (!fake && !data.$silenced && !data.$modified) ? '         	' : '') + (dispatch.base.protocolMap.code.get(code) || code) + '	' + data.toString('hex') + '\r\n', (function(){}))
 	})
 }
