9dd148c775738498b468c06c44e730b423c5750f
Owyn <Owyn@users.noreply.github.com>
2018-09-14 18:39:11 +0300

    +100500 fps and some tabulation

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = function DebugLogger(dispatch) {
 
 	dispatch.hook('*', 'raw', { order: 999999, filter: { fake: null, silenced: null, modified: null} }, (code, data, fromServer, fake) => {
 		let today = new Date();
-	fs.appendFileSync(file, "[" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds() + "]	" + (fromServer ? '<-' : '->') + '	' + (fake ? '[FAKE] ' : '       ') + (data.$silenced ? '[BLOCKED] ' : '') + (data.$modified ? '[MODIFIED] ' : '') + (dispatch.base.protocolMap.code.get(code) || code) + '	' + data.toString('hex') + '\n')
+	fs.appendFile(file, "[" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds() + "]	" + (fromServer ? '<-' : '->') + '	' + (fake ? '[CRAFTED]	' : '') + (data.$silenced ? '[BLOCKED]	' : '') + (data.$modified ? '[EDITED]	' : '') + ( (!fake && !data.$silenced && !data.$modified) ? '         	' : '') + (dispatch.base.protocolMap.code.get(code) || code) + '	' + data.toString('hex') + '\n', (function(){}))
 	})
 }
